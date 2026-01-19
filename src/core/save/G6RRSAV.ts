6f24594854ef054fc495c02133e1db95155cbe01
ArtemisWinter <artemiswinter.dev@protonmail.com>
2026-01-19 14:42:07 -0500

    Update G6RRSAV.ts

--- src/core/save/G6RRSAV.ts
+++ src/core/save/G6RRSAV.ts
@@ -13,19 +13,9 @@ export class G6RRSAV extends ORASSAV {
   static saveTypeName = 'Pokémon Rutile Ruby/Star Sapphire';
   static saveTypeID = 'G6RRSAV';
 
-  private static readonly originPluginMap: Record<OriginGame, string> = {
-    [OriginGame.OmegaRuby]: 'rutile_ruby',
-    [OriginGame.AlphaSapphire]: 'star_sapphire',
-  };
-
-  static getPluginIdentifier(origin?: OriginGame) {
-    if (origin && this.originPluginMap[origin]) {
-      return this.originPluginMap[origin];
-    }
-    return 'rutile_ruby_star_sapphire';
-  }
-
-  static getAllPluginIdentifiers() {
-    return Object.values(this.originPluginMap);
+  static getPluginIdentifier(origin: OriginGame) {
+    if (origin === OriginGame.OmegaRuby) return 'rutile_ruby';
+    if (origin === OriginGame.AlphaSapphire) return 'star_sapphire';
+    return 'rutile_ruby_star_sapphire'; // default fallback
   }
 }
