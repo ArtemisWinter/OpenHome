e9662cfa1c179c28629a2ea155aa641ffc9b9e23
ArtemisWinter <artemiswinter.dev@protonmail.com>
2026-01-19 12:33:42 -0500

    Update G6RRSAV.ts

--- src/core/save/G6RRSAV.ts
+++ src/core/save/G6RRSAV.ts
@@ -1,11 +1,22 @@
-import { ORASSAV } from './ORASSAV'
+import { PK6 } from '@pokemon-files/pkm'
+import { Box, BoxCoordinates } from './interfaces'
+import { ORAS_TRANSFER_RESTRICTIONS } from '@pokemon-resources/consts/TransferRestrictions'
+import { G6SAV } from './G6SAV'
 import { PathData } from './util/path'
 
-export class G6RRSAV extends ORASSAV {
+const PC_OFFSET = 0x33000
+const PC_CHECKSUM_OFFSET = 0x75fda
+const SAVE_SIZE_BYTES = 0x76000
+
+export class G6RRSAV extends G6SAV {
+  static transferRestrictions = ORAS_TRANSFER_RESTRICTIONS
   static saveTypeAbbreviation = 'Rutile Ruby'
   static saveTypeName = 'Pokémon Rutile Ruby'
   static saveTypeID = 'G6RRSAV'
-  static transferRestrictions = ORAS_TRANSFER_RESTRICTIONS
+  
+  static fileIsSave(bytes: Uint8Array): boolean {
+    return bytes.length === SAVE_SIZE_BYTES
+  }
 
   static getPluginIdentifier() {
     return 'rutile_ruby'
