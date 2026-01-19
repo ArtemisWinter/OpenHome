ec7e46b268a49b08d885dcbdfda397797281b381
ArtemisWinter <artemiswinter.dev@protonmail.com>
2026-01-19 14:10:10 -0500

    Update G6RRSAV.ts

--- src/core/save/G6RRSAV.ts
+++ src/core/save/G6RRSAV.ts
@@ -1,7 +1,7 @@
 import { OriginGame } from '@pkm-rs/pkg'
 import { PK6 } from '@pokemon-files/pkm'
 import { Box, BoxCoordinates } from './interfaces'
-import { G6SAV } from './G6SAV'
+import { ORASSAV } from './ORASSAV'
 import { PathData } from './util/path'
 
 const PC_OFFSET = 0x33000
@@ -15,12 +15,5 @@ export class G6RRSAV extends ORASSAV {
 
   static getPluginIdentifier() {
     return 'rutile_ruby_star_sapphire';
-  }
-
-  static includesOrigin(origin: OriginGame) {
-    return (
-      origin === OriginGame.RutileRuby ||
-      origin === OriginGame.StarSapphire
-    );
   }
 }
